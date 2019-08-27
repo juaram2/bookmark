7d744becbcfca2a7ab8a2331adcf16ee2b6bbc21
juaram2 <aaadkfka@naver.com>
2019-08-27 19:55:10 +0900

    북마크 axios

--- src/components/Bookmark.vue
+++ src/components/Bookmark.vue
@@ -1,36 +1,74 @@
 <template>
-  <div class="page-container md-layout-column">
-    bookmark
+  <div class="card-expansion">
+    <div v-for="bookmark of bookmarks" :key="bookmark.id">
+      <md-card >
+        <md-card-media>
+          <img :src="bookmarks.thumbnail" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">{{bookmark.title}}</div>
+          <div class="md-subhead">{{bookmark.url}}</div>
+        </md-card-header>
+
+        <md-button class="btn-close md-icon-button"><md-icon>close</md-icon></md-button>
+
+      </md-card>
+    </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+  export default {
+    name: 'Bookmark',
+    
+    data() {
+      return {
+        title: '',
+        url: '',
+        thumbnail: '',
+        bookmarks: [],
+      };
+    },
 
-export default {
-  name: 'Bookmark'
+    async created() {
+      try {
+        const res = await axios.get(`http://localhost:3000/bookmarks`)
+
+        this.bookmarks = res.data;
+      } catch(e) {
+        console.error(e)
+      }
+    },
+    // methods: {
+    //   getBookmarks () {
+    //     this.$http.get('http://localhost:3000/bookmarks')
+    //     .then((res) => {
+    //         this.bookmarks = res.data
+    //     })
+    //   }
+    // },
+    mounted(){
+      this.getBookmarks()
+    },
   }
+  
+
 </script>
 
 <style scoped>
-  @import url('//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons');
-
-  .page-container {
-    min-height: 300px;
-    overflow: hidden;
-    position: relative;
-    border: 1px solid rgba(#000, .12);
-  }
-
-  .md-drawer {
-    width: 230px;
-    max-width: calc(100vw - 125px);
-  }
-
-  .md-content {
-    padding: 16px;
-  }
-
-  .md-layout-item{
+  .md-card {
     width: 50%;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+  }
+
+  .btn-close{
+    position: absolute;
+    top: 0;
+    right: 0;
   }
 </style>
