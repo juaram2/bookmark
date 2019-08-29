0c83c98681549e169704091108d2fd4ad3d79ff6
juaram2 <aaadkfka@naver.com>
2019-08-29 19:40:38 +0900

    북마크 추가/삭제

--- src/components/Bookmark.vue
+++ src/components/Bookmark.vue
@@ -1,42 +1,43 @@
 <template>
-  <div class="card-expansion">
-    <div v-for="bookmark of bookmarks" :key="bookmark.id">
-      <md-card >
-        <md-card-media>
-          <img :src="bookmark.thumbnail" alt="People">
-        </md-card-media>
+  <div>
+    <div class="card-expansion" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="limit">
+      <div v-for="bookmark of bookmarks" :key="bookmark.id" data-aos="slide-up" data-aos-offset="100" data-aos-easing="ease-out-back">
+        <md-card >
+          <md-card-media>
+            <img :src="bookmark.thumbnail" alt="People">
+          </md-card-media>
 
-        <md-card-header>
-          <div class="md-title">{{bookmark.title}}</div>
-          <div class="md-subhead"><a :href="bookmark.url" target="_blank">{{bookmark.url}}</a></div>
-        </md-card-header>
+          <md-card-header>
+            <div class="md-title">{{bookmark.title}}</div>
+            <div class="md-subhead"><a :href="bookmark.url" target="_blank">{{bookmark.url}}</a></div>
+          </md-card-header>
 
-        <md-button class="btn-close md-icon-button"><md-icon>close</md-icon></md-button>
+          <md-button class="btn-close md-icon-button" v-on:submit.prevent="delBookmark(index)" @click="delBookmark(bookmark, index) in bookmarks"><md-icon>close</md-icon></md-button>
 
-      </md-card>
+        </md-card>
+      </div>
     </div>
 
-
-<!-- dialog -->
+    <!-- dialog -->
     <md-dialog :md-active.sync="showDialog">
       <md-dialog-title>New Bookmark</md-dialog-title>
       
       <md-field>
         <label>Title</label>
-        <md-input v-model="initial" placeholder="New Title"></md-input>
+        <md-input v-model="title" placeholder="New Title"></md-input>
       </md-field>
       <md-field>
         <label>URL</label>
-        <md-input v-model="initial" placeholder="http://url"></md-input>
+        <md-input v-model="url" placeholder="http://url"></md-input>
       </md-field>
       <md-field>
         <label>Thumbnail</label>
-        <md-input v-model="initial" placeholder="http://url/image"></md-input>
+        <md-input v-model="thumbnail" placeholder="http://url/image"></md-input>
       </md-field>
 
       <md-dialog-actions>
         <md-button class="md-primary" @click="showDialog = false">Close</md-button>
-        <md-button class="md-primary" @click="showDialog = false">Save</md-button>
+        <md-button class="md-primary" @click="showDialog = false" v-on:click="addBookmark">Save</md-button>
       </md-dialog-actions>
     </md-dialog>
 
@@ -48,6 +49,8 @@
 
 <script>
   import axios from 'axios';
+  const baseURL = "http://localhost:3000/bookmarks/"
+
   export default {
     name: 'Bookmark',
     
@@ -63,47 +66,65 @@
 
     async created() {
       try {
-        const res = await axios.get(`http://localhost:3000/bookmarks`)
-
+        const res = await axios.get(baseURL)
         this.bookmarks = res.data;
       } catch(e) {
         console.error(e)
       }
     },
-    // methods: {
-    //   getBookmarks () {
-    //     this.$http.get('http://localhost:3000/bookmarks')
-    //     .then((res) => {
-    //         this.bookmarks = res.data
-    //     })
-    //   }
-    // },
-    mounted(){
-      this.getBookmarks()
+
+    methods: {
+      // 북마크 추가
+      async addBookmark() {
+        const res = await axios.post(baseURL, { id:'', title: this.title, url: this.url, thumbnail: this.thumbnail })
+        this.bookmarks = [...this.bookmarks, res.data]
+      },
+      // 북마크 삭제
+      delBookmark(bookmark, id) {
+        axios.delete(baseURL + bookmark.id)
+        .then(response => {
+          this.bookmarks.splice(id, 1)
+          // location.reload();
+        });
+      },
+
     },
   }
-  
-
 </script>
 
 <style scoped>
+  .card-expansion {overflow: hidden;}
   .md-card {
     width: calc(50% - 8px);
+    height: 255.5px;
     margin: 4px;
     display: block;
     position: relative;
     float: left;
+  }
+  .md-card-media {
+    width: 100%;
+    height: 163.5px;
+  }
+  .md-card-media img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover
   }
   .md-card .md-subhead{
     font-size: 12px;
     width: 100%;
     overflow: hidden;
     text-overflow: ellipsis;
+    white-space: nowrap;
   }
-
+  .md-icon {
+    color: #fff!important;
+    text-shadow: 1px 1px 5px rgba(0, 0, 0, .5);
+  }
   .btn-close{
     position: absolute;
-    top: 0;
+    top: 6px;
     right: 0;
   }
   .md-fab {
